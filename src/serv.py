d54a26c9cdcd8ed127745744d6e6a6724bdfb328
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2021-04-16 02:26:20 +0200

    Ajout du header Access-Control-Allow-Origin pour permettre les requetes http en local

--- src/serv.py
+++ src/serv.py
@@ -37,6 +37,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print("\n")
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type','text/html')
         self.end_headers()
         urlP = urllib.parse.urlparse(self.path)

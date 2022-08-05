7325cfa27fcbc987ae9d32cfe9aafd0540bb78d9
casse <60-casse@users.noreply.022e47118ec0>
2022-08-05 14:52:53 +0200

    Re-organization and copyright added.

--- data/obviews/obviews.js
+++ data/obviews/obviews.js
@@ -1,4 +1,26 @@
-// main widget management
+/*
+ *	Obviews JS script
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2022, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+/****** Application state ******/
 const MODE_NOTHING = 0;
 const MODE_FUNCTION = 1;
 const MODE_SOURCE = 2;
